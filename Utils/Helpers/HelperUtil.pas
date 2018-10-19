1a7465c155a378462fc8cb612e0a73c1d3514e5e
Renan Lucilio <renanlucilio@gmail.com>
2018-10-19 18:11:05 -0300

    Adicionado primeiro Helper

---
+++ Utils/Helpers/HelperUtil.pas
@@ -0,0 +1,24 @@
+unit HelperUtil;
+
+interface
+
+uses
+  System.SysUtils;
+
+type
+  THelperDouble = record helper for Double
+  function formataDinheiro: string;
+
+
+  end;
+
+implementation
+
+{ THelperDouble }
+
+function THelperDouble.formataDinheiro: string;
+begin
+  Result:= FormatFloat('R$ ###,###,##0.00', Self);
+end;
+
+end.
